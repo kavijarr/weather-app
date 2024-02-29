47c5f544f7e2bfc3bcb6641b4d6389c7771e8b1b
kavijarr <kavijakumuditha12@gmail.com>
2024-02-29 20:20:35 +0530

    adding forecast

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import './WeatherCard.css';
 
 export default function WeatherCard() {
   const [isUpdated, setIsUpdated] = useState(true);
@@ -7,7 +8,7 @@ export default function WeatherCard() {
   useEffect(() => {
     fetch("http://api.weatherapi.com/v1/forecast.json?key=11cf9cef62ad418bad424730242502&q=Kegalle&days=3&aqi=no&alerts=no")
       .then((response) => response.json())
-      .then((data) => setWeather(data))
+      .then((data) =>{setWeather(data); console.log(data);})
       .catch((error) => console.error(error));
   }, []);
 
@@ -22,6 +23,20 @@ export default function WeatherCard() {
             </div>
             <div>
               <p className="display-6">feels like: {weather.current.feelslike_c}</p>
+            </div>
+            <div className="row">
+              <div className='col rounded border-2 border border-dark me-2'>
+                {weather.forecast.forecastday[0].date}
+                <img className='ms-4' src={weather.forecast.forecastday[0].day.condition.icon} alt="" />
+                </div>
+              <div className='col rounded border-2 border border-dark me-2'>
+              {weather.forecast.forecastday[1].date}
+                <img className='ms-4' src={weather.forecast.forecastday[1].day.condition.icon} alt="" />
+              </div>
+              <div className='col rounded border-2 border border-dark'>
+              {weather.forecast.forecastday[2].date}
+                <img className='ms-4' src={weather.forecast.forecastday[2].day.condition.icon} alt="" />
+              </div>
             </div>
           </>
         ) : (
